f9e0352195ad87297e1e6afe76e4c4527674f317
Alif142 <mizan.alif190@gmail.com>
2025-05-21 05:47:42 +0600

    updated zsh.sh

--- remake/regentmux.sh
+++ remake/regentmux.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+sudo pacman -S tmux
+sudo cp /home/alif/dotfiles/.tmux.conf /home/alif
+tmux source .tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp /home/alif/dotfiles/tmux_workspace.sh /home/alif/.tmux/
