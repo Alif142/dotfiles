0e8511a6320db5c623628f584b9696956c7fa3e4
Alif142 <mizan.alif190@gmail.com>
2025-12-09 13:02:49 +0600

    dotfiles backup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,13 +7,13 @@ config.font = wezterm.font_with_fallback({
     "Meslo LGM Nerd Font",
 })
 config.font_size = 17
-config.color_scheme = 'rose-pine'
+config.color_scheme = 'Rosé Pine (Gogh)'
 config.show_tabs_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
-config.colors = {
-    background = "black"
-}
-config.window_background_opacity = 0.6
+--config.colors = {
+--    background = "black"
+--}
+config.window_background_opacity = 0.0
 config.window_close_confirmation = "NeverPrompt"
 return config
